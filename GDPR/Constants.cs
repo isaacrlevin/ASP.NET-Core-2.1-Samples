4b36be1a1b9d0f425f92642dd106801dd05406c5
Isaac Levin <isaac.r.levin@gmail.com>
2018-07-06 14:28:54 -0400

    Added GDPR, RazorClassLibraries, and ScaffoldedIdentity samples

---
+++ GDPR/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace GDPR
+{
+    internal static class Constants
+    {
+        public const string EssentialSec = "EssentialSec";
+        public const string NonEssentialMS = "NonEssentialMS";
+    }
+}
